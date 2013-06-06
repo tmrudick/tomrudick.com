04a5db0475f1f4fb94d1b7d62608d347700f1b22
Tom Rudick <tmrudick@gmail.com>
2013-06-06 16:33:04 -0400

    Adding email stats

---
+++ jobs/gmail.js
@@ -0,0 +1,34 @@
+var request = require('request');
+
+job('gmail_traffic', '20m', function(done) {
+    var url = 'https://docs.google.com/spreadsheet/pub?key=0AuxhddH4pqmRdDR2WUhJMGluY0J3bW9zWHNOR1VWLXc&output=csv';
+
+    request.get(url, function(err, res) {
+        var csv = res.body;
+        csv = csv.split('\n');
+
+        var received = [];
+        var sent = [];
+
+        csv.forEach(function(row, index) {
+            if (index === 0) { return; }
+
+            var values = row.split(',');
+            var timestamp = +values[0];
+            received.unshift({
+                x: timestamp,
+                y: +values[1]
+            });
+
+            sent.unshift({
+                x: timestamp,
+                y: +values[2]
+            });
+        });
+
+        done({
+            received: received,
+            sent: sent
+        });
+    });
+});
