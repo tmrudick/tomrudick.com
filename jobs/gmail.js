7b60f21ac90f42e30ff0ff42d20c6b815b3aa141
Tom Rudick <tmrudick@gmail.com>
2013-06-07 19:27:46 -0400

    Changing font and updating some text

--- jobs/gmail.js
+++ jobs/gmail.js
@@ -1,7 +1,7 @@
 var request = require('request');
 
 job('gmail_traffic', '20m', function(done) {
-    var url = 'https://docs.google.com/spreadsheet/pub?key=0AuxhddH4pqmRdDR2WUhJMGluY0J3bW9zWHNOR1VWLXc&single=true&gid=0&range=A1%3AC8&output=csv';
+    var url = 'https://docs.google.com/spreadsheet/pub?key=0AuxhddH4pqmRdDR2WUhJMGluY0J3bW9zWHNOR1VWLXc&single=true&gid=0&range=A1%3AC15&output=csv';
 
     request.get(url, function(err, res) {
         var csv = res.body;
